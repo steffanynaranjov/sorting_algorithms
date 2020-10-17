f33fafa1a8bae6ecea5818546a6b9530d56af34b
steffanynaranjov <1792@holbertonschool.con>
2020-10-17 17:50:27 +0000

    first

--- sort.h
+++ sort.h
@@ -23,5 +23,8 @@ void print_list(const listint_t *list);
 
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+void swap_nodes(listint_t **list, listint_t *x, listint_t *y);
 
 #endif
